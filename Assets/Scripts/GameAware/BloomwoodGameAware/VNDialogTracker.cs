4d497d9abeebdd64a9172bd1d9ec80bd139e7d1f
Erik Harpstead <eharpste@cs.cmu.edu>
2023-09-12 13:23:17 -0400

    Added ScreenRects for Dialog Options
    Dialog Options now report a screenRect which should correspond with where the option would be rendered on screen if it was drawn. Note that the options can show up in the meta data before they're drawn still, since they don't appear until the player clicks for the preceding dialog.

--- Assets/Scripts/GameAware/BloomwoodGameAware/VNDialogTracker.cs
+++ Assets/Scripts/GameAware/BloomwoodGameAware/VNDialogTracker.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json.Linq;
 using InkFungus;
 using Ink.Runtime;
 using Newtonsoft.Json;
+using System.Linq.Expressions;
 
 public class VNDialogTracker : MetaDataTrackable
 {
@@ -17,12 +18,13 @@ public class VNDialogTracker : MetaDataTrackable
     private RectTransform rectTransform;
     private NarrativeDirector narrativeDirector;
     private JArray currentChoices = new JArray();
+    private List<RectTransform> optionRects = new List<RectTransform>();
 
     // Start is called before the first frame update
     override protected void Start() {
         /*
          * This class should be placed on the VN SayDialog object in a visual novel scene so that the expected Transform structure exists underneath it.
-         */ 
+         */
         storyText = transform.Find("Panel/StoryText").GetComponent<Text>();
         characterText = transform.Find("Panel/NameBackground/NameText").GetComponent<Text>();
         rectTransform = transform.Find("Panel/StoryText").GetComponent<RectTransform>();
@@ -31,9 +33,34 @@ public class VNDialogTracker : MetaDataTrackable
         objectKey = "visualNovelText";
         frameType = MetaDataFrameType.Inbetween;
         screenRectStyle = ScreenSpaceReference.Custom;
+
+
+
+
+
         base.Start();
 
+
+        /* Dialog options exist at:
+         * Menu Dialogs/VN MenuDialog/ButtonGoup
+         *  OptionButton0
+         *  OptionButton0 (1)
+         *  OptionButton0 (2) 
+         */
+
     }
+
+    private List<RectTransform> GetOptionRects() {
+        GameObject dialogOptionsPanel = GameObject.Find("Menu Dialogs/VN MenuDialog/ButtonGroup");
+        List<RectTransform> ret = new List<RectTransform>();
+        foreach(Transform t in dialogOptionsPanel.transform) {
+            if (t.name.StartsWith("Option")) {
+                ret.Add(t.GetComponent<RectTransform>());
+            }
+        }
+        return ret;
+    }
+
 
     private void Story_onMakeChoice(Choice choice) {
         //Debug.Log(choice);
@@ -47,8 +74,21 @@ public class VNDialogTracker : MetaDataTrackable
 
     private JArray GetChoices() {
         currentChoices.Clear();
-        foreach(Choice ch in narrativeDirector.story.currentChoices) {
-            currentChoices.Add(ch.text);
+        if(optionRects.Count == 0) {
+            optionRects = GetOptionRects();
+        }
+        if(optionRects.Count == 0) {
+            Debug.LogWarning("No Option Rects found.");
+        }
+
+        for(int i = 0; i < narrativeDirector.story.currentChoices.Count; i++) {
+            Choice ch = narrativeDirector.story.currentChoices[i];
+            RectTransform rect = optionRects[i];
+            DepthRect screenRect = ScreenSpaceHelper.RectTransformToViewerScreenRect(rect);
+            JObject choice = new JObject();
+            choice["text"] = ch.text;
+            choice["screenRect"] = screenRect.ToJObject();
+            currentChoices.Add(choice);
         }
         return currentChoices;
         
